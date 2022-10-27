014e478ea18998de44034b09f622b84fa4f74baa
rdN <rodionmaltsev1999@gmail.com>
2022-10-27 17:09:22 +0400

    Initial commit. Base versions of TCP Server and Client

---
+++ tcp_server/source/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "tcp_server.h"
+
+int main(int argc, char* argv[]) {
+
+    TCPServer server {IPV::V4, 1337};
+
+    server.OnJoin = [](TCPConnection::pointer connection) {
+        std::cout << "User has joined the server: " << connection->GetUsername() << std::endl;
+    };
+
+    server.OnLeave = [](TCPConnection::pointer connection) {
+        std::cout << "User has left the server: " << connection->GetUsername() << std::endl;
+    };
+
+    server.OnClientMessage = [&server](const std::string& message) {
+        // Parse the message
+		std::cout << message;
+
+        // Send message to all clients
+        // server.Broadcast(message);
+    };
+
+    return server.Run();
+}
